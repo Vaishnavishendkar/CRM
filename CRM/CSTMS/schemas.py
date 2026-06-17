04af75a36dbe32b3a52df098d87c8de98e90492e
Vaishnavishendkar <vaishnavi.shendkar28@gmail.com>
2026-06-17 12:09:07 +0530

    First commit

--- CRM/CSTMS/schemas.py
+++ CRM/CSTMS/schemas.py
@@ -1,2 +1,12 @@
 from pydantic import BaseModel
 from typing import Optional
+
+
+
+def Register(BaseModel):
+    username: str
+    email: str
+    password: str
+    roll: str
+    created_at: Optional[str] = None
+    updated_at: Optional[str] = None
